01b3474829e17c847cc1ab4387ff567a02f11a41
Dimitri Kindelberger <dimitri.kindelberger@telecom-sudparis.eu>
2026-09-15 21:04:11 +0200

    import existing acr intro terraform state

--- acr.tf
+++ acr.tf
@@ -1,3 +1,8 @@
+import {
+  to = azurerm_container_registry.acr
+  id = "/subscriptions/3f1b9162-df76-414a-ae4f-bb40ff6d3216/resourceGroups/rg-terraform-state/providers/Microsoft.ContainerRegistry/registries/acrkindellab77"
+}
+
 resource "azurerm_container_registry" "acr" {
   name                = "acrkindellab77" 
   resource_group_name = "rg-terraform-state"
